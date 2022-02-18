18b70c6fbbbe941353113d1c69fd5c643330c185
FushiginaMishiranuHito <88193716+FushiginaMishiranuHito@users.noreply.github.com>
2022-02-18 21:00:33 +0700

    Files for FetchBitch

---
+++ BitchFetch/precache-manifest.ca1f3a61a24a3ae767aac36c3a35b44a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad53956c10043802b3d3c92b446eff98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffc52ff49a0fe73ca588",
+    "url": "/static/css/main.c941e6ca.chunk.css"
+  },
+  {
+    "revision": "2236b5ff793db5af11f7",
+    "url": "/static/js/2.8530df89.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.8530df89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffc52ff49a0fe73ca588",
+    "url": "/static/js/main.2c0f4d1c.chunk.js"
+  },
+  {
+    "revision": "f13d5fb20258b992088c",
+    "url": "/static/js/runtime-main.a6b2f021.js"
+  }
+]);
